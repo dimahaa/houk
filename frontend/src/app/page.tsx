5c1834688a8f5b103e9acfc087831e0cdc04bac2
dimahaa <dimah.kazemzadeh@gmail.com>
2025-06-02 03:39:15 +0330

    Add form-4 component and custom font setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/global/headers/header";
 import Accordion1 from "./components/unique/acoodions/acoodion-1/acoodion-1";
+import AddUser2 from "./components/unique/form-4/form-4";
 
 export default function Home() {
   return (
     <div className="page-main">
       <Header />
       <Accordion1 />
+      <AddUser2 />
     </div>
   );
 }
