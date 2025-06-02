010ff9b20f3f76c8313cad56afa11510a65ad2d7
dimahaa <dimah.kazemzadeh@gmail.com>
2025-06-02 07:09:05 +0330

    Initial commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "./components/global/headers/header";
 import Accordion1 from "./components/unique/acoodions/acoodion-1/acoodion-1";
 
-
 export default function Home() {
   return (
     <div className="page-main">
