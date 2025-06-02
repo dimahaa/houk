be7514f034d7c7f7f01efb43bd6e6dacbea39488
dimahaa <dimah.kazemzadeh@gmail.com>
2025-06-02 04:07:16 +0330

    Add form-5 component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/global/headers/header";
 import Accordion1 from "./components/unique/acoodions/acoodion-1/acoodion-1";
-
+import AddUser3 from "./components/unique/form-5/form-5";
 export default function Home() {
   return (
     <div className="page-main">
       <Header />
       <Accordion1 />
+      <AddUser3 />
     </div>
   );
 }
