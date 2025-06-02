97a35aa827939f5aa10a8eba0f2c7821a5aa57a4
dimahaa <dimah.kazemzadeh@gmail.com>
2025-06-02 07:06:51 +0330

    start

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,11 @@
 import Header from "./components/global/headers/header";
 import Accordion1 from "./components/unique/acoodions/acoodion-1/acoodion-1";
-import AddUser3 from "./components/unique/form-5/form-5";
-import AddUser2 from "./components/unique/form-4/form-4";
 
 export default function Home() {
   return (
     <div className="page-main">
       <Header />
       <Accordion1 />
-      <AddUser2 />
-      <AddUser3 />
     </div>
   );
 }
